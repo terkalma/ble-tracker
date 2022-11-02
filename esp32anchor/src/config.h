18a76e7ceec5107173c6ce3fa3010009040f1852
Robert Terkal <terkalma@gmail.com>
2022-11-02 20:52:35 +0100

    rtls implementation with 2 anchors and 1 tag

--- esp32anchor/src/config.h
+++ esp32anchor/src/config.h
@@ -3,17 +3,16 @@
 
 #include "Arduino.h"
 
-// leftmost two bytes below will become the "short address"
-char anchor_addr[23]{'8', '0', ':', '0', '0', ':', '5', 'B', ':', 'D', '5', ':', 'A', '9', ':', '9', 'A', ':', 'E', '2', ':', '9', 'C'};
-
-#define SPI_SCK 18
-#define SPI_MISO 19
-#define SPI_MOSI 23
-#define DW_CS 4
-
 // connection pins
 const uint8_t PIN_RST = 27; // reset pin
 const uint8_t PIN_IRQ = 34; // irq pin
 const uint8_t PIN_SS = 4;   // spi select pin
+
+IPAddress server(192, 168, 0, 234);
+
+#define WIFI_UN "UPC5501473"
+#define WIFI_PW "d7mvswshZddh"
+#define UNAME "tag"
+#define PWD "potato666"
 
 #endif
--- esp32anchor/src/main.cpp
+++ esp32anchor/src/main.cpp
@@ -1,76 +1,206 @@
-//anchor #1 setup
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
 
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
 
-// be sure to edit anchor_addr and select the previously calibrated anchor delay
-// my naming convention is anchors 1, 2, 3, ... have the lowest order byte of the MAC address set to 81, 82, 83, ...
+/*
+ * StandardRTLSAnchorMain_TWR.ino
+ *
+ * This is an example master anchor in a RTLS using two way ranging ISO/IEC 24730-62_2013 messages
+ */
 
-#include <SPI.h>
-#include "DW1000Ranging.h"
-#include "DW1000.h"
+#include <DW1000Ng.hpp>
+#include <DW1000NgUtils.hpp>
+#include <DW1000NgRanging.hpp>
+#include <DW1000NgRTLS.hpp>
 #include "config.h"
+#include "WiFi.h"
+#include <PubSubClient.h>
 
-//calibrated Antenna Delay setting for this anchor
-uint16_t Adelay;
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
 
-void newRange()
-{
-  //    Serial.print("from: ");
-  Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-  Serial.print(", ");
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("espClient", UNAME, PWD)) {
 
-#define NUMBER_OF_DISTANCES 1
-  float dist = 0.0;
-  for (int i = 0; i < NUMBER_OF_DISTANCES; i++) {
-    dist += DW1000Ranging.getDistantDevice()->getRange();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait .5 seconds before retrying
+      delay(500);
+    }
   }
-  dist = dist/NUMBER_OF_DISTANCES;
-  Serial.println(dist);
 }
 
-void newDevice(DW1000Device *device)
-{
-  Serial.print("Device added: ");
-  Serial.println(device->getShortAddress(), HEX);
+void connectToWifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_UN, WIFI_PW);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 }
 
-void inactiveDevice(DW1000Device *device)
-{
-  Serial.print("Delete inactive device: ");
-  Serial.println(device->getShortAddress(), HEX);
+// Extended Unique Identifier register. 64-bit device identifier. Register file: 0x01
+char EUI[] = "80:BB:CC:DD:EE:FF:00:01";
+char EUB[] = "80:BB:CC:DD:EE:FF:00:02";
+
+byte main_anchor_address[] = {0x01, 0x00};
+byte anchor_b[] = {0x02, 0x00};
+
+double range_self;
+double range_B;
+
+boolean received_B = false;
+
+byte target_eui[8];
+byte tag_shortAddress[] = {0x05, 0x00};
+uint16_t blink_rate = 200;
+
+uint16_t next_anchor = 2;
+
+device_configuration_t DEFAULT_CONFIG = {
+    false,
+    true,
+    true,
+    true,
+    false,
+    SFDMode::STANDARD_SFD,
+    Channel::CHANNEL_5,
+    DataRate::RATE_850KBPS,
+    PulseFrequency::FREQ_16MHZ,
+    PreambleLength::LEN_256,
+    PreambleCode::CODE_3
+};
+
+frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+    false,
+    true /* This allows blink frames */
+};
+
+void setup() {
+    // DEBUG monitoring
+    Serial.begin(115200);
+    Serial.println("### DW1000Ng-arduino-ranging-anchor " + String(ANCHOR_INDEX) + " ###");
+    // initialize the driver
+    DW1000Ng::initializeNoInterrupt(PIN_SS, PIN_RST);
+    Serial.println(F("DW1000Ng initialized ..."));
+    // general configuration
+    DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
+    DW1000Ng::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
+
+    if (ANCHOR_INDEX == 1) {
+      DW1000Ng::setEUI(EUI);
+    } else if (ANCHOR_INDEX == 2) {
+      DW1000Ng::setEUI(EUB);
+    }
+
+    DW1000Ng::setPreambleDetectionTimeout(64);
+    DW1000Ng::setSfdDetectionTimeout(273);
+    DW1000Ng::setReceiveFrameWaitTimeoutPeriod(5000);
+
+    DW1000Ng::setNetworkId(RTLS_APP_ID);
+
+    DW1000Ng::setDeviceAddress(ANCHOR_INDEX);
+
+    DW1000Ng::setAntennaDelay(16436);
+
+    Serial.println(F("Committed configuration ..."));
+    // DEBUG chip info and registers pretty printed
+    char msg[128];
+    DW1000Ng::getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
+    DW1000Ng::getPrintableDeviceMode(msg);
+    Serial.print("Device mode: "); Serial.println(msg);
+
+    if (ANCHOR_INDEX == 1) {
+      connectToWifi();
+      client.setServer(server, 1883);
+    }
 }
 
-void setup()
-{
+void mainAnchor() {
+  if(DW1000NgRTLS::receiveFrame()){
+        size_t recv_len = DW1000Ng::getReceivedDataLength();
+        byte recv_data[recv_len];
+        DW1000Ng::getReceivedData(recv_data, recv_len);
+
+
+        if(recv_data[0] == BLINK) {
+            DW1000NgRTLS::transmitRangingInitiation(&recv_data[2], tag_shortAddress);
+            DW1000NgRTLS::waitForTransmission();
+
+            RangeAcceptResult result = DW1000NgRTLS::anchorRangeAccept(NextActivity::RANGING_CONFIRM, next_anchor);
+            if(!result.success) return;
+            range_self = result.range;
+        } else if(recv_data[9] == 0x60) {
+            double range = static_cast<double>(DW1000NgUtils::bytesAsValue(&recv_data[10],2) / 1000.0);
+            if(received_B == false && recv_data[7] == anchor_b[0] && recv_data[8] == anchor_b[1]) {
+                range_B = range;
+                received_B = true;
+
+                String report = String(ANCHOR_INDEX) + String(":") + String(range_self) + String(":");
+                report += String(recv_data[7]) + String(":") + String(range_B);
+
+                if (!client.connected()) {
+                  reconnect();
+                }
+                client.publish("tag", report.c_str());
+                client.loop();
+            } else {
+                received_B = false;
+            }
+        }
+    }
+}
+
+void transmitRangeReport() {
+    byte rangingReport[] = {DATA, SHORT_SRC_AND_DEST, DW1000NgRTLS::increaseSequenceNumber(), 0,0, 0,0, 0,0, 0x60, 0,0 };
+    DW1000Ng::getNetworkId(&rangingReport[3]);
+    memcpy(&rangingReport[5], main_anchor_address, 2);
+    DW1000Ng::getDeviceAddress(&rangingReport[7]);
+    DW1000NgUtils::writeValueToBytes(&rangingReport[10], static_cast<uint16_t>((range_self*1000)), 2);
+    DW1000Ng::setTransmitData(rangingReport, sizeof(rangingReport));
+    DW1000Ng::startTransmit();
+}
+
+void anchor(int index) {
+  RangeAcceptResult result = DW1000NgRTLS::anchorRangeAccept(NextActivity::ACTIVITY_FINISHED, blink_rate);
+    if(result.success) {
+      delay(4); // Tweak based on your hardware
+      range_self = result.range;
+      transmitRangeReport();
+
+      String rangeString = "Range: "; rangeString += range_self; rangeString += " m";
+      rangeString += "\t RX power: "; rangeString += DW1000Ng::getReceivePower(); rangeString += " dBm";
+      Serial.println(rangeString);
+    }
+}
+
+void loop() {
+
   if (ANCHOR_INDEX == 1) {
-    Adelay = 16570;
-  } else if (ANCHOR_INDEX == 2) {
-    Adelay = 16570;
+    mainAnchor();
+  } else {
+    anchor(ANCHOR_INDEX);
   }
-
-  anchor_addr[1] = ANCHOR_INDEX + '0';
-  Serial.begin(115200);
-  delay(1000); //wait for serial monitor to connect
-  Serial.println(ANCHOR_INDEX);
-  Serial.println("Anchor config and start");
-  Serial.print("Antenna delay ");
-  Serial.println(Adelay);
-
-  //init the configuration
-  SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
-  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
-
-  // set antenna delay for anchors only. Tag is default (16384)
-  DW1000.setAntennaDelay(Adelay);
-
-  DW1000Ranging.attachNewRange(newRange);
-  DW1000Ranging.attachNewDevice(newDevice);
-  DW1000Ranging.attachInactiveDevice(inactiveDevice);
-
-  //start the module as an anchor, do not assign random short address
-  DW1000Ranging.startAsAnchor(anchor_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
-}
-
-void loop()
-{
-  DW1000Ranging.loop();
 }
