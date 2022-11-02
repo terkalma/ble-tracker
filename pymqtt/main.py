57604366450f9aede136f0f86e243b4896a9f7de
Robert Terkal <terkalma@gmail.com>
2022-11-02 21:23:35 +0100

    change py

--- pymqtt/main.py
+++ pymqtt/main.py
@@ -11,21 +11,23 @@ client = mqtt.Client("tagserver")
 
 
 a1 = Anchor(0, 0)
-a2 = Anchor(105, 0)
+a2 = Anchor(150, 0)
 t = Tag(a1, a2)
 
 def on_message(client, userdata, msg, properties=None):
     # print(msg.payload.decode('utf-8'))
-    components = msg.payload.decode('utf-8').split("|")        
+    components = msg.payload.decode('utf-8').split(":")        
     try:        
         tag_id = components[0]
-        anchor_id = int(components[1])
-        distance = abs(int(float(components[2]) * 100))
-        t.set_distance(anchor_id, distance)
-        if all(t.ms):
-            t.ms = [False, False]
-            x, y = t.calculate()
-            print(t.ds[0], t.ds[1], x, y)
+
+        for i in range((len(components) - 1) // 2):
+            anchor_id = int(components[1 + 2 * i])
+            distance = abs(int(float(components[1 + 2 * i + 1]) * 100))
+            t.set_distance(anchor_id, distance)
+            if all(t.ms):
+                t.ms = [False, False]
+                x, y = t.calculate()
+                print(anchor_id, t.ds[0], t.ds[1], x, y)
     except:
         print(components)
   
